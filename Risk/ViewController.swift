d37a4d2ceee63719919fe04b6bfa8373090ca447
Vincent Predoehl <vpredoehl@gmail.com>
2018-06-21 21:09:54 -0500

    Initial Commit

---
+++ Risk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Risk
+//
+//  Created by Vincent Predoehl on 6/21/18.
+//  Copyright © 2018 Vincent Predoehl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
